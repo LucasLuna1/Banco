20a2ef74e30e2379552da3b0334c14ead92cd14a
Lucas <lucas3814889446@gmail.com>
2024-07-03 19:21:54 -0300

    prueba fallada sin app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import ElNavbar from './componentes/navbar';
+import Footer from './componentes/Footer';
+import Principal from './componentes/principal';
+import Banking from './componentes/banking';
 
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <ElNavbar />
+        <Routes>
+          <Route path="/" element={<Principal />} />
+          <Route path="/banking" element={<Banking />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
 
 
 
